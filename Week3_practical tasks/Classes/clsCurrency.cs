8e4eaf38388c02bbebd3d160db89c270b9bfcaf9
Alan Burnell <WS1318@weston.ac.uk>
2020-10-12 11:02:14 +0100

    Set up class for currency - including variables for each currency and set/get requests for each currency.

---
+++ Week3_practical tasks/Classes/clsCurrency.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_practical_tasks.Classes
+{
+    public class clsCurrency
+    {
+        private double m_dEuro = 0.89;
+        private double m_dSterling = 1;
+        private double m_dDollar = 0.72;
+
+        public double GetSterling()
+        {
+            return m_dSterling;
+        }
+
+        public double GetEuro()
+        {
+            return m_dEuro;
+        }
+
+        public double GetDollar()
+        {
+            return m_dDollar;
+        }
+
+        public void SetSterling(double dSterling)
+        {
+            m_dSterling = dSterling;
+        }
+
+        public void SetEuro(double dEuro)
+        {
+            m_dEuro = dEuro;
+        }
+
+        public void SetDollar(double dDollar)
+        {
+            m_dDollar = dDollar;
+        }
+    }
+}
